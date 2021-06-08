a61e942e9f858d549f60f8cf1b1c9e0a286189df
Mellewanadoo <mellewanadoo@gmail.com>
2021-06-08 14:59:59 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Main} from "./src/Main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Main />
+    </SafeAreaView>
   );
 }
 
